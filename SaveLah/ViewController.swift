317bf5b650fb36996dad69698665d60c38ba069d
dyanezzatee <s10182484@connect.np.edu.sg>
2019-12-26 12:15:11 +0800

    Initial Commit

---
+++ SaveLah/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaveLah
+//
+//  Created by Dyanezzatee on 26/12/19.
+//  Copyright © 2019 DianIzzati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
